c84b5eb6f18c6d0be2701c9572ebf4d70520c8db
jck-y <s2200582@student.unklab.ac.id>
2025-09-17 16:59:29 +0800

    Add ACTIVE_ELECTION config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,1 @@
-// Single source of truth for the active election
-// You can override via env: VITE_ELECTION_ID
-export const ACTIVE_ELECTION =
-  import.meta.env.VITE_ELECTION_ID || "sma-osis-2025";
+export const ACTIVE_ELECTION = "sma-osis-2025";
